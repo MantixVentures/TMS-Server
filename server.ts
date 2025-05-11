bf6eadb74982cbcb9e6bb15d252e59e9b88a9a34
Dulshan-Siriwardhana <dulshan.s@botcalm.com>
2025-05-11 11:56:56 +0530

    up

--- server.ts
+++ server.ts
@@ -17,6 +17,9 @@ const PORT = 5000;
 app.use(cors());
 
 app.use(express.json());
+app.use("/", (req, res) => {
+    res.send("TMS Server is healthy");
+  });
 app.use("/users", userRoute);
 app.use("/admin", adminRoute);
 app.use("/policeIssueFine", policeIssueFineRoute);
